4c7f0ab35abfb7505b4ff6c01205f944636a18d9
Hetvi Chudasama <155105217+HetviChudasama@users.noreply.github.com>
2024-05-07 00:39:16 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+if (typeof document !== 'undefined') {
+    const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+    // Your code that interacts with the DOM here
+};
+
+
+const hamburger = document.querySelector(".header .nav-bar .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const header = document.querySelector("#header .header");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener('scroll',() =>{
+  var scroll_position = window.scrollY;
+  if(scroll_position > 250){
+    header.style.backgroundColor = "#29323c";
+  }
+    else{
+      header.style.backgroundColor = "transparent";
+    }
+
+});
+
+menu_item.forEach(item =>{
+  item.addEventListener("click" ,() =>{
+    hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+
+  });
+});
